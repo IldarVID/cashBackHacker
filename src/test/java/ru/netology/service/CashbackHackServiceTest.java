240fc5da253226a645021f2ec48d2a165ef92e25
VID Neto <dello5@yandex.ru>
2023-06-27 23:44:58 +0300

    junit4 branch and tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,20 +1,20 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void remainAmountToBuy() {
 
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 800;
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 800;
 
-        int actual = cashbackHackService.remain(amount);
-        int expected = 200;
+            int actual = cashbackHackService.remain(amount);
+            int expected = 200;
 
-          assertEquals(actual, expected);
+            assertEquals(actual, expected);
     }
 }
